ffa5305352488aa48b78892f26c27e16b07d4a25
JaeminShin <999fg@kaist.ac.kr>
2016-07-17 22:42:01 -0700

    FilterMakingConfirmActivity's format implemented. Design and server communication can be further updated.

--- Android/FilterShare/app/src/main/java/team16/filtershare/FilterMakingConfirmActivity.java
+++ Android/FilterShare/app/src/main/java/team16/filtershare/FilterMakingConfirmActivity.java
@@ -1,10 +1,13 @@
 package team16.filtershare;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by shinjaemin on 2016. 7. 6..
@@ -15,6 +18,13 @@ public class FilterMakingConfirmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_filter_make);
+
+        GlobalVariables sfApp = ((GlobalVariables)getApplicationContext());
+        String picturepath = sfApp.get_picture_path();
+        Bitmap bitimg = BitmapFactory.decodeFile(picturepath);
+
+        ImageView img = (ImageView) findViewById(R.id.test_image);
+        img.setImageBitmap(bitimg);
 
         final Button back_button = (Button) findViewById(R.id.back_button);
         back_button.setOnClickListener(new View.OnClickListener() {
