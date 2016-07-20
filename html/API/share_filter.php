cd7dee3c81604523b54a28b8b991ed0d78c33d6a
Josh Bernfeld <joshbernfeld@gmail.com>
2016-07-19 20:27:24 -0700

    update share filter to jay specification

--- html/API/share_filter.php
+++ html/API/share_filter.php
@@ -7,16 +7,27 @@ $tags = 	safePost('tags');
 $username = safePost('username');
 $device_id = safePost('device_id');
 $attributes = safePost('filter_attributes');
+$brightness = safePost('brightness');
+$contrast = safePost('contrast');
+$saturation = safePost('saturation');
+$sharpen = safePost('sharpen');
+$temperature = safePost('temperature');
+$tint = safePost('tint');
+$vignette = safePost('vignette');
+$grain = safePost('grain');
+$date_created = safePost('date_created');
 
-/*
-$name = 'Nice Filters';
+
+/*$name = 'Nice Filters';
 $username = 'JoshBernfeld';
 $device_id = 'somedeviceid';
 $tags = "#something #somethingmore";
-$attributes = array('sharpen' => 10, 'contrast' => 4);
+#$attributes = array('sharpen' => 10, 'contrast' => 4);
+$date_created = 9999;
+$brightness = 0.9;
 */
 
-if(!$name || !$username || !$device_id || !$attributes) {
+if(!$name || !$username || !$device_id) {
 	echo "Invalid Parameters";
 	exit();
 }
@@ -39,20 +50,22 @@ DB::getDB()->query("
 	atr_tint = :tint,
 	atr_vignette = :vignette,
 	atr_grain = :grain,
-	date_created = UTC_TIMESTAMP()
+	date_created = :date_created,
+	server_date_created = UTC_TIMESTAMP()
 ",
 array(
 'name'			=> $name,
 'username' 		=> $username,
 'device_id' 	=> $device_id,
-'brightness' 	=> arrayItem($attributes, 'brightness'),
-'contrast' 		=> arrayItem($attributes, 'contrast'),
-'saturation'	=> arrayItem($attributes, 'saturation'),
-'sharpen' 		=> arrayItem($attributes, 'sharpen'),
-'temperature' 	=> arrayItem($attributes, 'temperature'),
-'tint' 			=> arrayItem($attributes, 'tint'),
-'vignette' 		=> arrayItem($attributes, 'vignette'),
-'grain' 		=> arrayItem($attributes, 'grain')));
+'brightness' 	=> $brightness,
+'contrast' 		=> $contrast,
+'saturation'	=> $saturation,
+'sharpen' 		=> $sharpen,
+'temperature' 	=> $temperature,
+'tint' 			=> $tint,
+'vignette' 		=> $vignette,
+'grain' 		=> $grain,
+'date_created'  => $date_created));
 
 $filter_id = DB::getDB()->connection->lastInsertId();
 
