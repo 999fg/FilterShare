53017e55100264f41ddc69c0dc18dea8d6c9a52f
Josh Bernfeld <joshbernfeld@gmail.com>
2016-07-22 04:57:02 -0700

    return tags order by recent

--- html/API/filter_query.php
+++ html/API/filter_query.php
@@ -5,14 +5,6 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/API/class/class_helper.php');
 $device_id = safePost('device_id');
 $data_paging = safePost('data_paging');
 
-/*
-$name = 'Nice Filters';
-$username = 'JoshBernfeld';
-$device_id = 'somedeviceid';
-$tags = "#something #somethingmore";
-$attributes = array('sharpen' => 10, 'contrast' => 4);
-*/
-
 /*if(!$name || !$username || !$device_id || !$attributes) {
 	echo "Invalid Parameters";
 	exit();
@@ -23,26 +15,50 @@ if(!$data_paging) {
 }
 
 $filter_query = DB::getDB()->query("
-SELECT
-filter_id,
-name,
-username,
-date_created,
-atr_brightness as brightness,
-atr_contrast as contrast,
-atr_saturation as saturation,
-atr_sharpen as sharpen,
-atr_temperature as temperature,
-atr_tint as tint,
-atr_vignette as vignette,
-atr_grain as grain
-FROM filters
-LIMIT :start, 5
+	SELECT
+	filter_id,
+	name,
+	username,
+	date_created,
+	atr_brightness as brightness,
+	atr_contrast as contrast,
+	atr_saturation as saturation,
+	atr_sharpen as sharpen,
+	atr_temperature as temperature,
+	atr_tint as tint,
+	atr_vignette as vignette,
+	atr_grain as grain
+	FROM filters
+	ORDER BY filter_id DESC
+	LIMIT :start, 5
 ",
-array(
-'start'			=> $data_paging));
+array('start' => $data_paging));
 
-$filter_array = $filter_query ->fetchAll(PDO::FETCH_ASSOC);
+$filter_array = $filter_query->fetchAll(PDO::FETCH_ASSOC);
+
+
+foreach($filter_array as $key => $filter) {
+	$final_tags = array();
+	
+	$tag_query = DB::getDB()->query("
+		SELECT
+		tag
+		FROM filter_tags
+		WHERE filter_id = :filter_id
+	",
+	array(
+	'filter_id'			=> $filter['filter_id']));
+	
+	$tag_array = $tag_query->fetchAll(PDO::FETCH_ASSOC);
+	
+	foreach($tag_array as $tag) {
+		$final_tags[] = $tag['tag'];
+	}
+	
+	$filter['tags'] = $final_tags;
+	
+	$filter_array[$key] = $filter;
+}
 
 $data_paging = $data_paging+count($filter_array);
 
